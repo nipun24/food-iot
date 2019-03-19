205b9dfb4d273d0f5cab865674579aa5d89603ea
Nipun Haldar <nipunhaldar24@gmail.com>
2019-03-19 23:04:43 +0530

    added all necessary information from the app to the backend

--- server/index.js
+++ server/index.js
@@ -26,18 +26,34 @@ app.post('/token', (req,res) => {
 
 //send all the items
 app.get('/list', (req,res) => {
-  db.collection('food').find().toArray((err,results) => {
-    res.status(200).send(results)
+  db.collection('food').find({"barcode":"1234"}).toArray((err,results) => {
+    if(results.length === 0)
+      res.status(200).send(results)
+    else
+      res.status(200).send('yes')
   })
 })
 
 //add items to database
 app.post('/add',(req,res) => {
-  db.collection('food').insertOne(req.body,(err,result) => {
-    if (err) throw err
-    res.status(200).send(true)
+  db.collection('food').find({"barcode": req.body.barcode}).toArray((err,results) => {
+    if(results.length === 0){
+      db.collection('food').insertOne({
+        "name": req.body.name,
+        "barcode": req.body.barcode,
+        "mfd": [req.body.mfd],
+        "exp": req.body.exp,
+        "timeToExp": [req.body.timeToExp]
+      },(err,result) => {
+        if (err) throw err
+        res.status(200).send(true)
+      })
+    }
+    else{
+      db.collection('food').updateOne({"barcode":`${req.body.barcode}`},{$push:{"mfd":req.body.mfd}})
+      res.status(200).send(true)
+    }
   })
-
 })
 
 app.listen(PORT, () => console.log(`app is listening on port ${PORT}!`))
